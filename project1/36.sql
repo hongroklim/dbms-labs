1ba7fc377a37a1080d65ae1b3b90329ab7521744
임홍록 <2018007710@hanyang.ac.kr>
2021-09-26 11:45:42 +0900

    update project1/36.sql

--- project1/36.sql
+++ project1/36.sql
@@ -6,4 +6,9 @@ SELECT P.name
           FROM Evolution E
          WHERE E.before_id = P.id
        )
+   AND EXISTS
+       (SELECT NULL
+          FROM Evolution E
+         WHERE E.after_id = P.id
+       )
  ORDER BY P.name;
